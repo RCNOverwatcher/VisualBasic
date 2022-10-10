05856b86bf727dfe8b901f7f101723889c3c22d5
Jacob <jacobjameswiltshire@protonmail.com>
2022-10-10 15:10:46 +0100

    Test problems 2 and 3

--- VBTestProblem2/Program.vb
+++ VBTestProblem2/Program.vb
@@ -2,44 +2,22 @@ Imports System
 
 Module Program
     Sub Main(args As String())
-        Dim names(5) as String
+        Dim names() as String = {"Harry", "Joe", "Susan", "Joanna", "Michael", "John"}
+        Dim linkedListNames as New LinkedList(Of String)(names)
+        Dim node as LinkedListNode(Of String) = linkedListNames.First
+        Do While node IsNot Nothing
+            Console.WriteLine(node.Value)
+            node = node.Next
+        Loop
         
-        names(0) = "Harry"
-        names(1) = "Joe"
-        names(2) = "Susan"
-        names(3) = "Joanna"
-        names(4) = "Michael"
-        names(5) = "John"
+        Console.WriteLine("Sorted list")
+        array.Sort(names)
+        Dim linkedListNamesSorted as New LinkedList(Of String)(names)
+        node = linkedListNamesSorted.First
+        Do While node IsNot Nothing
+            Console.WriteLine(node.Value)
+            node = node.Next
+        Loop
         
-        Array.Sort(names)
-        
-        Dim nameLinkedList As New LinkedList(of String)
-        
-        For counter = 0 to 5
-            nameLinkedList.Append(names(counter))
-        Next
-        
-        
-        If IsListEmpty(nameLinkedList) = True
-            Console.WriteLine("The Linked list is empty!")
-        Else
-            Console.WriteLine("Linked list not empty!")
-        End If
     End Sub
-    
-    Function IsListEmpty(byVal linkedList as LinkedList(Of String))
-        Dim sArray(linkedList.Count) as String
-        
-        linkedList.CopyTo(sArray, 0)
-        
-        If sArray.Length <=1 Then
-            Return True
-        Else 
-            Return False
-        End If
-    End Function
-    
-    Function AddToLinkedList(ByRef linkedList As LinkedList(Of String))
-        
-    End Function
 End Module
